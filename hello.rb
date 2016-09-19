ad4b0a354bb2ceb882012d8e701611e1b19ff55c
Ito Takumi <e125743@ie.u-ryukyu.ac.jp>
2016-09-19 05:21:17 +0000

    Update hello.rb

--- hello.rb
+++ hello.rb
@@ -1,5 +1,12 @@
+# -*- coding: utf-8 -*-
 require 'sinatra'
 
 get '/' do
   "<H1>Hello World, AllT</H1>"
+end
+
+get '/hello/:name' do
+  # "GET /hello/foo" と "GET /hello/bar" にマッチ
+  # params['name'] は 'foo' か 'bar'
+  "Hello #{params['name']}!"
 end
